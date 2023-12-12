b551c3c4d9ced3c2afccd91829a278a04fcfbab7
Andreas Maier <andreas.maier-1@uni-hamburg.de>
2023-12-12 23:18:43 +0100

    fixed for cuda 12.2 and xinference 0.7.1

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,13 @@
 FROM andimajore/mamba_nvidia_ubuntu:latest
 
 RUN apt-get update && apt-get install -y build-essential
-RUN mamba install python=3.9
+RUN mamba install python=3.10
 RUN mamba install -c conda-forge cxx-compiler
 
 # Install all xinference dependencies
-RUN pip install xinference[all]
+RUN pip install xinference[all]==0.7.1
 
-RUN mamba install git
+RUN mamba install -y pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch-nightly -c nvidia
 
 WORKDIR /opt/conda/lib/python3.9/
 
--- register_and_launch.sh
+++ register_and_launch.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-sleep 5
-xinference register --persist --file /cosybio/project/LLM/xinference_configs/llama2-13b-chat-hf.json
+sleep 10
+xinference register --persist --file /cosybio/project/LLM/xinference_configs/llama2-13b-chat-hf-untouched.json
+xinference register --persist --file /cosybio/project/LLM/xinference_configs/llama2-13b-hf-untouched.json
 xinference launch --model-name llama2-13b-chat-hf --size-in-billions 13 --model-format pytorch --quantization none --n-gpu 1
-xinference launch --model-name qwen-chat --size-in-billions 14 --model-format pytorch --quantization none --n-gpu 1
+xinference launch --model-name llama2-13b-hf --size-in-billions 13 --model-format pytorch --quantization none --n-gpu 1
 xinference launch --model-name gte-large --model-type embedding
+xinference launch --model-name bge-reranker-large --model-type rerank
