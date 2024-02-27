1b910933242ca9cad998b93b1a0fa76a7744537f
Andreas Maier <andreas.maier-1@uni-hamburg.de>
2024-02-27 15:35:07 +0100

    latest startup and Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,22 @@
 FROM andimajore/mamba_nvidia_ubuntu:latest
 
 RUN apt-get update && apt-get install -y build-essential
+RUN apt-get install libnccl2 libnccl-dev
 RUN mamba install python=3.10
-RUN mamba install -c conda-forge cxx-compiler
+RUN mamba install -c conda-forge cxx-compiler diffusers
 
 # Install all xinference dependencies
-RUN pip install xinference[all]==0.8.4
+#RUN pip install llama-cpp-python==0.2.20
+#ARG CMAKE_ARGS="-DLLAMA_CUBLAS=on" pip install llama-cpp-python
+#RUN pip install xinference[all]==0.8.4
+RUN pip install xinference[transformers]==0.9.0
+#RUN pip install xinference[vllm]==0.9.0
 #RUN mamba install git
 #RUN git clone https://github.com/AndiMajore/inference.git
 
 RUN mamba install -y pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch-nightly -c nvidia
+#RUN python -m cupyx.tools.install_library --library nccl --cuda 12.1
+RUN pip install sentence-transformers
 
 WORKDIR /opt/conda/lib/python3.9/
 
--- register_and_launch.sh
+++ register_and_launch.sh
@@ -7,8 +7,10 @@ fi
 #xinference register --persist --file /cosybio/project/LLM/xinference_configs/llama2-13b-hf-untouched.json
 #xinference register --persist --file /cosybio/project/LLM/xinference_configs/phi-2.json
 #xinference register --persist --file /cosybio/project/LLM/xinference_configs/mixstral-8x7B-instruct.json
-xinference launch --model-name llama-2-chat --size-in-billions 13 --model-format pytorch --quantization none  --n-gpu 1
+#xinference launch --model-name llama-2-chat --size-in-billions 13 --model-format pytorch --quantization none  --n-gpu 1
 #xinference launch --model-name llama2-13b-hf --size-in-billions 13 --model-format pytorch --quantization none --n-gpu 1
-xinference launch --model-name mixtral-instruct-v0.1 --size-in-billions 46_7 --model-format pytorch -u mixtral-8x7B-instruct-v01 --n-gpu 3
+#xinference launch --model-name mixtral-instruct-v0.1 --size-in-billions 46_7 --model-format pytorch -u mixtral-8x7B-instruct-v01 --n-gpu 4
+xinference launch --model-name llama-2-chat --size-in-billions 13 --model-format pytorch --quantization none  --n-gpu 2
+xinference launch --model-name gemma-it --size-in-billions 7 --model-format pytorch --quantization none --n-gpu 1
 xinference launch --model-name gte-large --model-type embedding
 xinference launch --model-name bge-reranker-large --model-type rerank
